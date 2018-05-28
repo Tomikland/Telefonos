8224fc1308d991dc6e6bd333772b13d957f68614
Tomikland <tomikland@gmail.com>
2018-05-28 17:19:33 +0200

    Add files via upload

---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public enum TileType {Path, Buildable, UnBuildable};
+
+    public TileType tileType = TileType.Buildable;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
