330e62330305a8a8ad3bd61b03d7e59b46d6324e
Tomikland <tomikland@gmail.com>
2018-05-29 23:49:50 +0200

    Add files via upload

---
+++ Script/PathFinder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PathFinder  {
+
+    static public List<Tile> FindPath(Tile[,] tiles,Tile src, Tile dest)
+    {
+        List<Tile> path = new List<Tile>();
+
+        Tile currTile = src;
+        path.Add(src);
+
+
+        int count = 0;
+        do
+        {
+            count++;
+
+            foreach (Tile n in TileHelper.Neighbours4(currTile))
+            {
+                if(n.tileType == TileType.Path && path.Contains(n) == false)
+                {
+                    path.Add(n);
+
+                    //Debug.Log("Added tile"+n.x +"_"+n.y);
+
+                    currTile = n;
+                    break;
+                }
+            }
+
+        }
+        while (currTile != dest && count < 100);
+        Debug.Log("Path found in " + count + " iterations");
+
+            return path;
+    }
+
+
+}
---
+++ Script/TileHelper.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TileHelper {
+
+    static Tile[,] tiles;
+
+	public static Vector3 TilePosition(Tile t)
+    {
+        return new Vector3(t.x, t.y , 0);
+    }
+
+    public static Tile TileUnderPos(Vector2 pos)
+    {
+        return tiles[Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y)];
+    }
+
+    public static List<Tile> Neighbours4(Tile t)
+    {
+        List<Tile> neighbours = new List<Tile>();
+
+        if(t.x != 0)
+        {
+            neighbours.Add(tiles[t.x - 1, t.y]);
+        }
+        if (t.x != tiles.GetLength(0))
+        {
+            neighbours.Add(tiles[t.x + 1, t.y]);
+        }
+        if (t.y != 0)
+        {
+            neighbours.Add(tiles[t.x , t.y-1]);
+        }
+        if (t.y != tiles.GetLength(1))
+        {
+            neighbours.Add(tiles[t.x , t.y + 1]);
+        }
+
+        return neighbours;
+    }
+
+    public static void SetTileArray (Tile[,] t)
+    {
+        tiles = t;
+    }
+
+    public static Vector2 RoundVector(Vector2 a)
+    {
+        return new Vector2(Mathf.Round(a.x) , Mathf.Round(a.y));
+    }
+    
+    public static Vector2 PredictEnemyPos(Enemy enemy, float t)
+    {
+
+        //Debug.Log(enemy.path.Count);
+
+        {
+       //     Debug.Log(enemy.gameObject.name);
+        }
+        Vector2 pos = enemy.transform.position;
+        Vector2 pos2 = RoundVector(pos);
+        Vector2 remainder_pos = pos2 - pos;
+        float full = enemy.speed * t;
+        int index = Mathf.RoundToInt(enemy.speed * t);
+        float remainder = full - index;
+        Vector2 endDir = 
+            Mathf.Sign(remainder) == -1 && index + enemy.currIndex <= enemy.path.Count ? 
+            enemy.path[index + enemy.currIndex].Position() - enemy.path[(index - 1) + enemy.currIndex].Position() 
+            : 
+            enemy.path[(index + 1) + enemy.currIndex].Position() - enemy.path[index + enemy.currIndex].Position();
+        Tile endTile;   
+            endTile = enemy.path[index + enemy.currIndex];
+        Vector2 result = endTile.Position() + remainder_pos + (endDir * remainder);
+        return result;
+    }
+}
